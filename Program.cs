2cf2a1471f7d1e74cb8933a558bc2984fd686e6b
V8275 <87564199+V8275@users.noreply.github.com>
2025-09-28 18:25:10 +0300

    added project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ReservationApp
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+        }
+    }
+}
